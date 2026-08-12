2c824110f939f1d92693004ac16baece66848194
airy <airy@ya.ru>
2026-08-12 22:28:30 +0300

    ordering files

--- home_work_01/09_ex.py
+++ home_work_01/09_ex.py
@@ -2,7 +2,7 @@ numbers = [10, -5, 7, -3, 15, 0, 8]
 print("All numbers from 0 array")
 for number in numbers:
     print(number)
-print("Only numbers from 0 to 10")
+print("Only numbers > 0")
 for number in reversed(numbers):
-    if number >= 0:
+    if number > 0:
         print(number)
