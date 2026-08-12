f62232e3137baa7fdecb42c011710cfe7fa6db5b
airy <airy@ya.ru>
2026-08-12 22:47:34 +0300

    HW_01 fixed output in 09_ex

--- home_work_01/09_ex.py
+++ home_work_01/09_ex.py
@@ -1,8 +1,8 @@
 numbers = [10, -5, 7, -3, 15, 0, 8]
-print("All numbers from 0 array")
+print("\nAll numbers from 0 array: ", numbers)
+
+result = []
 for number in numbers:
-    print(number)
-print("Only numbers > 0")
-for number in reversed(numbers):
     if number > 0:
-        print(number)
+        result.append(number)
+print("\nOnly numbers > 0: ", result)
