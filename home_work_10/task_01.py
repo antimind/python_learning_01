3d3f968d39c56207898871724dde111a5a2f216e
airy <airy@ya.ru>
2026-09-18 19:24:06 +0300

    hw-10 fixed task 1

--- home_work_10/task_01.py
+++ home_work_10/task_01.py
@@ -1,13 +1,11 @@
 text = "Python используется для веб-разработки.\nPython подходит для автоматизации.\nИзучать Python интересно."
-line_count = 0
 with open("article.txt", "w", encoding='utf_8') as file:
     file.write(text)
 
 with open("article.txt", "r", encoding='utf_8') as file:
     text_from_file = file.read()
 
-    for line in file:
-        line_count += 1
+    line_count = len(text_from_file.split("\n"))
 
     print(f"All text:\n{text_from_file}\n\n")
     print(f"Count of strings: {line_count},\nCount of chars: {len(text_from_file)}")
